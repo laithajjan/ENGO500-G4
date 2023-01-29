2522298e3839ffc1f8332550f7b35792535b19fc
Connor <connorbgood11@gmail.com>
2023-01-28 18:34:40 -0700

    Restructured files to avoid errors that occurred because of the directories not being python modules. Each directory should contain their relevant python modules now. Small changes to update paths in the ProcessInputToCSV.py and reformatted the input structure for readability.

--- DataPreprocessing/Script/DataPreprocessing.py
+++
@@ -1,5 +0,0 @@
-from DataPreprocessing.Classes.AccumapCsvReader import AccumapCsvReader
-
-# Script to perform Data preprocessing
-
-# Read in the data from unprocessed csv downloaded from Accumap, probably best to read all .csv file in a folder together
